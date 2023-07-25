f0b2fccd609b9870d1e65b8a8473c6814cd8da15
ceceusyu2023 <cecendululuusyu@gmail.com>
2023-07-25 03:52:34 -0700

    main.h

--- main.h
+++ main.h
@@ -30,13 +30,13 @@ struct fmt
 };
 
 /**
- * struct fmt fmt_t - Struct op
+ *typedef struct fmt fmt_t - struct op
  *
  * @fmt: The format
  * @fmt_t: The function associated
- *
- * Return: Number of printed chars
  */
+typedef struct(fmt) fmt_t;
+
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 		va_list list, char buffer[], int flags, int width, int precision, int size);
